fb4926f0f426bb74781fd0bd390b8752567d70a6
toyu1610 <toyu1610@gmail.com>
2026-09-25 08:50:29 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,1 @@
-<?php
-$host     = getenv('DB_HOST')     ?: 'localhost';
-$user     = getenv('DB_USER')     ?: 'root';
-$password = getenv('DB_PASSWORD') ?: '';
-$dbname   = getenv('DB_NAME')     ?: 'vfinancial';
-$port     = getenv('DB_PORT')     ?: 3306;
 
-$conn = mysqli_connect($host, $user, $password, $dbname, (int)$port);
-
-if (!$conn) {
-    die("Koneksi Database Gagal: " . mysqli_connect_error());
-}
-?>
